f7d8a48c9a1e34fe9feb2035ac886f7ab51591f2
Nikolas Mikolaski <mikolaskin@gmail.com>
2022-08-17 15:22:33 -0400

    +Added alternate dividing function (works) +Changed parameters of divide function so that it accepts one vector --Need to fix return function, is endless loop of return.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,7 @@
 using std::vector;
 
 
-//function that takes array and loops continually while diving array until theres is only 1 number left
-int* divideArray(int* masterArray, int* arrayLeft, int* arrayRight)
-{
-	return 0;
-}
+
 //function that sorts the numbers in the array
 int* sortArray(int* unsortedLeft, int * arrayRight, int arrayLength)
 {
@@ -24,7 +20,7 @@ int* sortArray(int* unsortedLeft, int * arrayRight, int arrayLength)
 	return 0;
 }
 //function that merges two arrays together into 1 array
-void mergeArray(vector<int>& mergedVector, vector<int> arrayLeft, vector<int> arrayRight)
+vector<int> mergeArray(vector<int> arrayLeft, vector<int> arrayRight)
 {
 	//if the value in the left array is less than the value in the right, append it to the vector and use the next left value
 	//if the value in the right array is less than the value in the left array, append the value to the vector and continue down the right array
@@ -51,6 +47,9 @@ void mergeArray(vector<int>& mergedVector, vector<int> arrayLeft, vector<int> ar
 	}
 	*/
 	//test faster merge loop
+
+	vector<int> mergedVector;
+
 	int left_index = 0;
 	int right_index = 0;
 
@@ -69,13 +68,69 @@ void mergeArray(vector<int>& mergedVector, vector<int> arrayLeft, vector<int> ar
 			right_index++;
 		}
 	}
+
+	return mergedVector;
+}
+//function that takes array and loops continually while diving array until theres is only 1 number left
+vector<int> divideArray(vector<int> unsortedArray)
+{
+	vector<int> arrayLeft, arrayRight;
+
+	while (unsortedArray.size() % 2)
+	{
+
+		for (int i = 0; i < unsortedArray.size(); i++)
+		{
+			arrayLeft.push_back(unsortedArray[i]);
+			arrayRight.push_back(unsortedArray[i + (unsortedArray.size() / 2)]);
+		}
+		return divideArray(arrayLeft);
+	}
+	return unsortedArray;
+}
+
+vector<int> seperateArray(vector<int> unsortedArray)
+{
+	vector<int> arrayLeft, arrayRight;
+
+	//if the unsorted array is not a single number, continue to divide array
+	if (unsortedArray.size() != 1)
+	{
+		//when dividing the array, if the array is an odd number add the remainder at the end making the left side larger. otherwise divide equally.
+		for (int i = 0; i < unsortedArray.size() / 2; i++)
+		{
+			arrayLeft.push_back(unsortedArray[i]);
+			//start the right array at the midpoint plus modulus if there is one so ti is smaller.
+			arrayRight.push_back(unsortedArray[i + (unsortedArray.size() / 2) + (unsortedArray.size()%2)]);
+		}
+		if (unsortedArray.size() % 2)
+		{
+			arrayLeft.push_back(unsortedArray[unsortedArray.size()/2]);
+		}
+	}
+	return mergeArray(seperateArray(arrayLeft), seperateArray(arrayRight)); //NEED TO FIX
+
 }
 
 void main()
 {
+
+	vector<int> unsortedArray = { 1,8,3,2,6,4 };
 	vector<int> mergedArray;
 	vector<int> arrayLeft = { 1, 3, 8 };
 	vector<int> arrayRight = { 2, 4, 6 };
-
-	mergeArray(mergedArray, arrayLeft, arrayRight);
+	mergedArray = seperateArray(unsortedArray);
+	mergeArray(arrayLeft, arrayRight);
 }
+
+/*pseudocode
+-while an array is not a single number, continue to divide the array
+divideArray()
+{
+while array.size()!=1
+divide array
+else
+return a merged array
+}
+when the array is has become a single entity, merge it into the correct numeric order
+*/
